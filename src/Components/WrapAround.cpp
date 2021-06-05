562048ff4843330007b93d3f3d2e6bc96108386b
Amanda de Moura <5531103+dmdemoura@users.noreply.github.com>
2021-06-05 15:56:01 -0300

    fix: WrapAround bug

--- src/Components/WrapAround.cpp
+++ src/Components/WrapAround.cpp
@@ -4,6 +4,14 @@
 #include "Transform.hpp"
 #include <memory>
 
+namespace
+{
+    float Mod(float a, float b)
+    {
+        return std::signbit(a) ? std::fmod(a + 1.0f, b) + b - 1.0f : std::fmod(a, b);
+    }
+}
+
 auto WrapAround::Clone() const -> std::unique_ptr<Component>
 {
     return std::make_unique<WrapAround>();
@@ -15,8 +23,8 @@ auto WrapAround::VDrawUpdate() -> void
     {
         transform->localPosition = Vector3
         {
-            std::fmod(transform->localPosition.x + 1.0f, 2.0f) - 1.0f,
-            std::fmod(transform->localPosition.y + 1.0f, 2.0f) - 1.0f,
+            Mod(transform->localPosition.x + 1.0f, 2.0f) - 1.0f,
+            Mod(transform->localPosition.y + 1.0f, 2.0f) - 1.0f,
             transform->localPosition.z
         };
     }
