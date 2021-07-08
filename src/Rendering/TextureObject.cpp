bdbc0d18265d9a5ce9d3dc987302e856b2878aac
Luis Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2021-07-08 20:54:54 -0300

    More scenere

--- src/Rendering/TextureObject.cpp
+++ src/Rendering/TextureObject.cpp
@@ -37,7 +37,7 @@ auto TextureObject::UploadTexture(GLint level, const Texture2D& textureAsset) ->
     // Refactor this code into their own functions.
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glGenerateMipmap(GL_TEXTURE_2D);
 }
